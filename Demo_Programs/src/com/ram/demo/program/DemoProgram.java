abf978ebba5740140703caf6271886b206e62a26
Ramen2k10 <ramen2k10@gmail.com>
2019-08-17 00:10:58 +0530

    first commit

---
+++ Demo_Programs/src/com/ram/demo/program/DemoProgram.java
@@ -0,0 +1,15 @@
+package com.ram.demo.program;
+
+public class DemoProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	    getDisplay("Ramen");
+	}
+
+	private static void getDisplay(String name) {
+     System.out.println("This is a demo program created by "+name);		
+	}
+	
+	
+}
